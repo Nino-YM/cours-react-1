b1eb248b87559546fd49dbf1eb2f6cbfb5e08091
Nino-YM <119847747+Nino-YM@users.noreply.github.com>
2024-07-10 16:03:15 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque voluptate, odit animi corrupti tempore nobis iure voluptas nostrum vero, molestiae fugit sit, mollitia error libero perspiciatis. Possimus corrupti dolores doloremque, nobis alias voluptate neque! Ipsam animi reiciendis modi voluptatibus ab repellat, incidunt consequuntur. Repellendus pariatur ullam quibusdam commodi dolore laboriosam accusantium! Sit atque error aut voluptatibus deserunt perferendis facere odit fugit exercitationem eligendi? Corrupti est cupiditate accusantium fugiat culpa molestias eaque blanditiis in, nostrum iure nihil delectus nulla qui tempora? Commodi beatae dolorem omnis et, quibusdam voluptatem, iusto sit deserunt nobis ipsam quas nesciunt ex aut. Quos cum placeat eum?</p>
+            <br />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In consectetur earum ducimus deleniti, neque totam, nesciunt possimus iure quia quisquam unde saepe ratione quo dignissimos nemo consequatur quasi dolor recusandae qui natus officiis nam? Saepe blanditiis quas totam ullam perspiciatis impedit et dolore. Culpa sed saepe architecto quasi unde sit optio vero ipsum illo cupiditate eum earum laudantium, facere necessitatibus quae officia. Placeat, velit quo, in odio ut, odit cumque deserunt possimus provident repudiandae iure quibusdam magni beatae inventore molestiae facilis quia labore nam ad quisquam officia laboriosam ab? Aut id quod harum optio culpa? Inventore, neque? Dolorem, odit aperiam magnam assumenda, amet, placeat repellendus explicabo sequi rerum quaerat repudiandae accusamus expedita nobis cumque. Accusamus quasi nesciunt corrupti perspiciatis dolore veritatis iusto earum ut facilis, odio fugiat perferendis, cumque aliquam placeat molestiae rem officia iste natus hic repudiandae nihil sint necessitatibus pariatur ullam? Ratione quo illum necessitatibus, dignissimos aut vitae ad vel autem nisi, quis rem excepturi fugiat impedit dolorum sed vero blanditiis assumenda non laboriosam reiciendis numquam reprehenderit rerum. Minima nemo quia asperiores reprehenderit quidem quasi ad? Corrupti animi nemo quaerat? Voluptate molestias blanditiis exercitationem aut nam illum? Assumenda veritatis iure voluptates, animi odit dicta perspiciatis placeat maiores expedita?</p>
+        </div>
+    );
+};
+
+export default About;
